edec078009a0c47f4d854122511d7cc2a4d30921
Walter Liendo <walter.liendo@gmail.com>
2025-01-13 19:41:30 -0300

    invisibilice

--- src/components/Firebase.js
+++ src/components/Firebase.js
@@ -1,55 +1,20 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore, collection, getDocs, getDoc, doc, query, where, addDoc } from "firebase/firestore";
 import { GoogleAuthProvider, getAuth } from 'firebase/auth';
-import { deleteObject, ref, getStorage, getDownloadURL, uploadBytesResumable } from 'firebase/storage';
-
 
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_API_KEY,
     authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    databaseURL: process.env.REACT_APP_DATABASEURL,
     projectId: process.env.REACT_APP_PROJECTID,
-    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    // storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
     messagingSenderId: process.env.REACT_APP_MESSAGING_SENDERID,
     appId: process.env.REACT_APP_APPID,
-    measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 export const auth = getAuth(app);
-export const storage = getStorage(app);
 export const googleProvider = new GoogleAuthProvider();
-
-export const DeleteFile = async (fileRef) => {
-    try {
-      await deleteObject(fileRef);
-      console.log("Archivo eliminado correctamente");
-    } catch (error) {
-      console.error("Error al eliminar archivo:", error);
-      throw error; // Puedes manejar el error según tu lógica de aplicación
-    }
-  };
-
-export const uploadFile = (file, imageRef, setProgress, setRemoteImg) => {
-    const storageRef = ref(storage, imageRef);
-    const upload = uploadBytesResumable(storageRef, file);
-    upload.on(
-      'state_changed',
-        (snapshot) => {
-            const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-            setProgress(progress);
-        },
-        (error) => {
-           console.log(error);
-        },
-        () => {
-            getDownloadURL(upload.snapshot.ref).then((downloadURL) => {
-                setRemoteImg({ url: downloadURL, imageRef: imageRef });
-            });
-        }
-    );
-}
 
 
 export async function getAllProducts() {
